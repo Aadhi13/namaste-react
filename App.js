6db103ff42044a99928f8cf7632a144a92188808
Adhil Ameen <67848808+Aadhi13@users.noreply.github.com>
2024-04-04 06:28:25 +0530

    Introduced Functional Components.

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namste React");
-
-const jsxHeading = <h1 id="jsxHeading" >Namste React</h1>;
-
-console.log(heading, "react element using core React.js");
-console.log(jsxHeading, "react element using JSX");
+const HeadingComponent = () => {
+	return <h1>Namste React functional Component</h1>;
+};
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
