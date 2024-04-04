d4f840a9c8b932cf70bde728368522fd78215902
Adhil Ameen <67848808+Aadhi13@users.noreply.github.com>
2024-04-04 09:14:48 +0530

    Functional Components part 2.

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const HeadingComponent = () => {
-	return <h1>Namste React functional Component</h1>;
-};
+const TitleComponent = () => (
+	<h1 className="title">
+		Namste React Title.
+	</h1>
+);
+
+const HeadingComponent = () => (
+	<>
+		<TitleComponent />
+		<h1>Namste React Functional Component</h1>
+	</>
+);
 
 const root = createRoot(document.getElementById("root"));
 
